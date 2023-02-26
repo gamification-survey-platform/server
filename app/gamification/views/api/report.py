c0419f68218204ee29462b768d6c3e8124d2c025
TonyRays <haichuan16888@126.com>
2023-02-26 15:05:30 -0800

    feat: report page

--- app/gamification/views/api/report.py
+++ app/gamification/views/api/report.py
@@ -32,64 +32,69 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 class ViewReport(generics.ListCreateAPIView):
     # queryset = Entity.objects.all()
     serializer_class = EntitySerializer
-    permission_classes = [permissions.AllowAny] # [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
-    def get(self, request, course_id, assignment_id, andrew_id, *args, **kwargs):
-        user = get_object_or_404(CustomUser, andrew_id=andrew_id)
-        course = get_object_or_404(Course, pk=course_id)
-        registration = get_object_or_404(
-            Registration, users=user, courses=course)
-        userRole = registration.userRole
-        assignment = get_object_or_404(Assignment, pk=assignment_id)
-        assignment_type = assignment.assignment_type
-        if assignment_type == "Individual":
+    def get(self, request, course_id, assignment_id, *args, **kwargs):
+        if 'andrew_id' in request.query_params:
+            andrew_id = request.query_params['andrew_id']
+            user = get_object_or_404(CustomUser, andrew_id=andrew_id)
+            course = get_object_or_404(Course, pk=course_id)
+            registration = get_object_or_404(
+                Registration, users=user, courses=course)
+            userRole = registration.userRole
+            assignment = get_object_or_404(Assignment, pk=assignment_id)
+            assignment_type = assignment.assignment_type
+            if assignment_type == "Individual":
+                try:
+                    entity = Individual.objects.get(
+                        registration=registration, course=course)
+                    team_name = str(andrew_id)
+                except Individual.DoesNotExist:
+                    # Create an Individual entity for the user
+                    individual = Individual(course=course)
+                    individual.save()
+                    membership = Membership(student=registration, entity=individual)
+                    membership.save()
+                    entity = Individual.objects.get(
+                        registration=registration, course=course)
+                    team_name = str(andrew_id)
+            elif assignment_type == "Team":
+                try:
+                    entity = Team.objects.get(registration=registration, course=course)
+                    team_name = entity.name
+                except Team.DoesNotExist:
+                    # Alert: you need to be a member of the team to upload the artifact, forbidden
+                    return Response("You are not a member of the team", status=status.HTTP_403_FORBIDDEN)
+            else:
+                return Response("Assignment type is not valid", status=status.HTTP_400_BAD_REQUEST)
+            # find artifact id with assignment id and entity id
+            # artifact = get_object_or_404(Artifact, assignment=assignment, entity=entity)
             try:
-                entity = Individual.objects.get(
-                    registration=registration, course=course)
-                team_name = str(andrew_id)
-            except Individual.DoesNotExist:
-                # Create an Individual entity for the user
-                individual = Individual(course=course)
-                individual.save()
-                membership = Membership(student=registration, entity=individual)
-                membership.save()
-                entity = Individual.objects.get(
-                    registration=registration, course=course)
-                team_name = str(andrew_id)
-        elif assignment_type == "Team":
-            try:
-                entity = Team.objects.get(registration=registration, course=course)
-                team_name = entity.name
-            except Team.DoesNotExist:
-                # Alert: you need to be a member of the team to upload the artifact, forbidden
-                return Response("You are not a member of the team", status=status.HTTP_403_FORBIDDEN)
+                artifact = Artifact.objects.get(assignment=assignment, entity=entity)
+                artifact_exists_flag = True
+                artifact_id = artifact.pk
+                artifact_path = artifact.file.url
+                artifact_url = r"/api/artifacts/" + str(artifact_id) + "/"
+                artifact_answers_url = r"/api/artifacts/" + str(artifact_id) + r"/answers/statistics"
+            except Artifact.DoesNotExist:
+                print("artifact does not exist")
+                artifact_exists_flag = False
+                artifact_id = None
+                artifact_path = None
+                artifact_url = None
+                artifact_answers_url = None
+            context = {'user': user,
+                    'course': course,
+                    'entity': entity,
+                    'userRole': userRole,
+                    'artifact_url': artifact_url,
+                    'artifact_path': artifact_path,
+                    'team_name': team_name,
+                    "artifact_exists_flag": artifact_exists_flag,
+                    "artifact_answers_url": artifact_answers_url
+                    }
+            data = json.dumps(context)
+            return Response(data)
         else:
-            return Response("Assignment type is not valid", status=status.HTTP_400_BAD_REQUEST)
-        # find artifact id with assignment id and entity id
-        # artifact = get_object_or_404(Artifact, assignment=assignment, entity=entity)
-        try:
-            artifact = Artifact.objects.get(assignment=assignment, entity=entity)
-            artifact_exists_flag = True
-            artifact_id = artifact.pk
-            artifact_path = artifact.file.url
-            artifact_url = r"/api/artifacts/" + str(artifact_id) + "/"
-            artifact_answers_url = r"/api/artifacts/" + str(artifact_id) + r"/answers/statistics"
-        except Artifact.DoesNotExist:
-            print("artifact does not exist")
-            artifact_exists_flag = False
-            artifact_id = None
-            artifact_path = None
-            artifact_url = None
-            artifact_answers_url = None
-        context = {'user': user,
-                'course': course,
-                'entity': entity,
-                'userRole': userRole,
-                'artifact_url': artifact_url,
-                'artifact_path': artifact_path,
-                'team_name': team_name,
-                "artifact_exists_flag": artifact_exists_flag,
-                "artifact_answers_url": artifact_answers_url
-                }
-        data = json.dumps(context)
-        return Response(data)
+            # missing andrew_id
+            return Response("Missing andrew_id", status=status.HTTP_400_BAD_REQUEST)
