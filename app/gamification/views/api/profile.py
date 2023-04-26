7395791cce9642a78156db3c2ae46af459da6a47
Vishaal Agartha <vishaalagartha@gmail.com>
2023-04-26 14:35:07 -0700

    Add profile picture logic

--- app/gamification/views/api/profile.py
+++ app/gamification/views/api/profile.py
@@ -13,12 +13,34 @@ from django.conf import settings
 from app.gamification.utils import get_user_pk
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from app.gamification.utils import generate_presigned_url, generate_presigned_post
 
 
 class UserProfile(generics.RetrieveUpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny]
+
+
+    def update_profile_picture(self, request, user):
+        picture = request.FILES.get('image')
+        if not picture:
+            return None
+
+        content_type = picture.content_type
+        if content_type == 'image/jpeg':
+            file_ext = 'jpg'
+        elif content_type == 'image/png':
+            file_ext = 'png'
+        else:
+            return None
+        key = picture
+        if settings.USE_S3:
+            key = f'profile_pics/user_{user.id}.{file_ext}'
+        user.image = key
+        user.save()
+
+        return key
 
     @swagger_auto_schema(
         operation_description='Get user information by andrew id',
@@ -36,25 +58,6 @@ class UserProfile(generics.RetrieveUpdateAPIView):
             )
         }
     )
-    def create_profile_picture(self, request, user):
-        picture = request.FILES.get('image')
-        if not picture:
-            return None
-
-        content_type = picture.content_type
-        if content_type == 'image/jpeg':
-            file_ext = 'jpg'
-        elif content_type == 'image/png':
-            file_ext = 'png'
-        else:
-            return None
-
-        if settings.USE_S3:
-            key = f'users/user_{user.id}.{file_ext}'
-        else:
-            key = picture
-
-        return key
 
     def get(self, request, *args, **kwargs):
         user_id = get_user_pk(request)
@@ -63,8 +66,18 @@ class UserProfile(generics.RetrieveUpdateAPIView):
             'first_name': user.first_name,
             'last_name': user.last_name,
             'email': user.email,
-            'image': user.image
         }
+
+                # Generate the presigned URL to share with the user.
+        if settings.USE_S3 and user.image != None:
+            upload_url = generate_presigned_post(user.image)
+            download_url = generate_presigned_url(user.image, http_method='GET')
+            user_info['image'] = download_url
+        elif user.image != None:
+            upload_url = f'http://{settings.ALLOWED_HOSTS[1]}:8000{user.image.url}'
+            download_url = f'http://{settings.ALLOWED_HOSTS[1]}:8000{user.image.url}'
+            user_info['image'] = download_url
+
         return Response(user_info)
 
     @swagger_auto_schema(
@@ -78,30 +91,39 @@ class UserProfile(generics.RetrieveUpdateAPIView):
                 'email': openapi.Schema(type=openapi.TYPE_STRING)
             }
         )
-
     )
+
     def patch(self, request, *args, **kwargs):
         user_id = get_user_pk(request)
         user = CustomUser.objects.get(id=user_id)
         first_name = request.data.get('first_name')
         last_name = request.data.get('last_name')
         email = request.data.get('email')
-        image = request.data.get('image')
+
         if first_name:
             user.first_name = first_name
         if last_name:
             user.last_name = last_name
         if email:
             user.email = email
-        if image:
-            image_key = self.create_profile_picture(request, user)
-            user.image = image_key
         user.save()
         user_info = {
             'first_name': user.first_name,
             'last_name': user.last_name,
             'email': user.email,
         }
-        if user.image:
-            user_info['image'] = user.image
+
+        key = self.update_profile_picture(request, user)
+
+        # Generate the presigned URL to share with the user.
+        if settings.USE_S3 and key != None:
+            upload_url = generate_presigned_post(key)
+            download_url = generate_presigned_url(key, http_method='GET')
+            user_info['image'] = download_url
+        elif key != None:
+            upload_url = f'http://{settings.ALLOWED_HOSTS[1]}:8000{user.image.url}'
+            download_url = f'http://{settings.ALLOWED_HOSTS[1]}:8000{user.image.url}'
+            user_info['image'] = download_url
+
         return Response(user_info)
+    
