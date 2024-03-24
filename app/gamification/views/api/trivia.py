4ff5dea832d3969ca300d70c1d287a5255adf974
rhuang4F2023 <rhuang4@andrew.cmu.edu>
2024-03-23 17:09:18 -0700

    completed

--- app/gamification/views/api/trivia.py
+++ app/gamification/views/api/trivia.py
@@ -18,19 +18,24 @@ from app.gamification.utils.auth import get_user_pk
 from app.gamification.utils.levels import inv_level_func, level_func
 from app.gamification.models.user_trivia import UserTrivia
 
+from django.http import JsonResponse
 
 class TriviaView(generics.GenericAPIView):
     serializer_class = TriviaSerializer
     permission_classes = [AllowAny]
+
     def get(self, request, course_id):
         user_id = get_user_pk(request)
         user = get_object_or_404(CustomUser, pk=user_id)
         course = get_object_or_404(Course, pk=course_id)
-        # Get trivia items that have not been completed by the user
+        all_trivia_qs = Trivia.objects.filter(course=course)
         completed_trivia_ids = UserTrivia.objects.filter(user=user, is_completed=True).values_list('trivia', flat=True)
-        trivia_qs = Trivia.objects.filter(course=course).exclude(id__in=completed_trivia_ids)
-        if trivia_qs.exists():
-            serializer = TriviaSerializer(trivia_qs, many=True)
-            return Response(serializer.data)
+        remaining_trivia_qs = all_trivia_qs.exclude(id__in=completed_trivia_ids)
+
+        if not all_trivia_qs.exists():
+            return JsonResponse({"message": "No trivia for this course"}, status=status.HTTP_204_NO_CONTENT)
+        elif not remaining_trivia_qs.exists():
+            return JsonResponse({"message": "All trivia completed"}, status=status.HTTP_208_ALREADY_REPORTED)
         else:
-            return Response({"message": "You have completed all trivia for this course"}, status=status.HTTP_204_NO_CONTENT)
+            serializer = TriviaSerializer(remaining_trivia_qs, many=True)
+            return Response(serializer.data)
